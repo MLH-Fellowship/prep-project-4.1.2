d86c193abd00110c24c44229dafda837661e6c00
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-15 20:42:39 +0530

    docs: add appropriate comments for the setup

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -6,7 +6,22 @@ import { Router, Route, Switch } from 'react-router-dom';
 // Assets
 import history from './utils/createBrowserHistory';
 
-// Lazily Load Pages (Helps with code splitting)
+/**
+ * React has a feature where the production code can
+ * be splitted into chunks instead of one single file
+ * which helps tremendously for reducing loading times
+ * - the function 'lazy' splits the code as per different pages
+ *   i.e. the code for home page loads first then for the other pages
+ * - the component 'Suspense' helps to show a loading component
+ *   when the code is being fetched asynchronously
+ * - lazy and Suspense should always be used simultaneously
+ *
+ * => Steps to add new pages
+ * 1. As show below, create a component using lazy function.
+ *    Add the location of the page to be loaded.
+ * 2. Inside the Switch component, add a new Route component with
+ *    the appropriate component and the link for it.
+ */
 const AsyncWeather = lazy(() => import('./pages/Weather'));
 const AsyncError = lazy(() => import('./pages/Error'));
 
