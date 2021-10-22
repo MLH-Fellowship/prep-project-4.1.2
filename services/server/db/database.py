0c4c6e744227e7b26023f909fd7e888a54888f9f
Nikhil B N <nikhilbn365@gmail.com>
2021-10-22 19:52:24 +0530

    Update Dockerfile
    Signed-off-by: Nikhil B N <nikhilbn365@gmail.com>

--- services/server/db/database.py
+++ services/server/db/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
-    SQLALCHEMY_DATABASE_URL = DATABASE_URL
+    SQLALCHEMY_DATABASE_URL = "postgresql" + DATABASE_URL[8:]
 else:
     SQLALCHEMY_DATABASE_URL = "postgresql://{user}:{password}@{host}/{db}".format(
         user=os.environ.get('POSTGRES_USER'),
